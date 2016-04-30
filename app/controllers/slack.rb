f3367fd0928147c6a9ecaa7d78e0171428fc42de
Rafal Cymerys <rafal@cymerys.com>
2016-04-30 21:43:18 +0200

    Add executing entry command

--- app/controllers/slack.rb
+++ app/controllers/slack.rb
@@ -3,7 +3,24 @@ module Controllers
     post '/' do
       request_validator.validate_request(params)
 
-      'testcior'
+      parser = Parser::EntryCommand.new
+      harvest = Service::Harvest.new
+
+      parsed_command = parser.parse(params[:text])
+
+      project_lookup = Lookup::Project.new(harvest)
+      project = project_lookup.find(parsed_command.project)
+
+      task_lookup = Lookup::Task.new(project)
+      task = task_lookup.find(parsed_command.task)
+
+      command = Command::SwitchEntry.new(harvest, project_id: project.id, task_id: task.id,
+                                         notes: parsed_command.notes, hours_ago: parsed_command.hours)
+
+      command.execute
+
+
+      "Now billing #{project.name} #{task.name}"
     end
 
     private
